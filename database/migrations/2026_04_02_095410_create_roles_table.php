2e745ecfa6d5116a1bbad8d30c8bd829b9d53022
Mehdi Benchekroun <benchekrounmehdi2006@gmail.com>
2026-04-03 15:38:02 +0100

    Creaction des modeles

--- database/migrations/2026_04_02_095410_create_roles_table.php
+++ database/migrations/2026_04_02_095410_create_roles_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-         Schema::create('roles', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name'); // client, freelancer, admin
+            $table->string('name');
         });
     }
 
